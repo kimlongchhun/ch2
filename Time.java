4ee2a60e306c3b5c0828d85b9c15993b1f15eace
kimlongchhun <kimlong.chhun168@gmail.com>
2026-09-14 14:49:58 -0700

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,13 @@
+public class Time{
+	public static void main(String[] args) {
+		int hour = 15;
+		int minute = 60;
+		int second = 60;
+		System.out.println(hour * minute * second);
+		
+		
+		
+	}
+	
+}
+
