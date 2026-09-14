eefe9308b94037023f22a3f6858ea6d453bf4258
kimlongchhun <kimlong.chhun168@gmail.com>
2026-09-14 14:44:17 -0700

    Add files via upload

---
+++ date.java
@@ -0,0 +1,9 @@
+public class date{
+	public static void main(String[] args) {
+		int day = 14;
+		int month = 9;
+		int year = 2026;
+		System.out.println(day + "/" + month + "/" + year);		
+		
+}
+}
