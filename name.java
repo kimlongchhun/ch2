f4bfecd3e3b00314912483fb8e6060976bd09bcc
kimlongchhun <kimlong.chhun168@gmail.com>
2026-09-14 14:36:02 -0700

    Add files via upload

---
+++ name.java
@@ -0,0 +1,11 @@
+public class name {
+	public static void main(String[] args) {
+	String name = "Kimlong";
+	int age = 16;
+	System.out.println("My" + " name" + " is " + name + " and " + " I " + " am " + age);
+	System.out.println (7 / 2);
+	System.out.println (7.0 / 2);
+	System.out.println (7 % 2);
+	
+}
+}
